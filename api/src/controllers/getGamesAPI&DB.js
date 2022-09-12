0361f1015be7a841feadce6b00593b399cd2806a
dante-dlcio <erio-donalicio@hotmail.com>
2022-09-12 20:40:29 +0200

    'changes saved'

--- api/src/controllers/getGamesAPI&DB.js
+++ api/src/controllers/getGamesAPI&DB.js
@@ -4,23 +4,12 @@ const { Videogame } = require('../db');
 const {API_KEY} = process.env;
 
 
-const getAllGames = async () => {
-    let promises = [];
-    for(let i = 1; i<=5; i++){
-        const games = axios.get(
-            `https://api.rawg.io/api/games?${API_KEY}?page${i}`
-        );
-        promises.push(games)
+const getVideogames = async () => {
+    try {
+        
+    } catch (error) {
+        
     }
-    return await Promise.all(promises)
-    .then((responses)=>{
-        let pages = responses.map(r=> r?.data?.results)
-        let result = pages.filter((x)=> x != null).flat().map((g)=>{
-            return{
-                id: g.id,
-                name: g.id
-            }
-        })
-        return result;
-    })
 }
+    
+module.exports = { getVideogames };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,11 +1,14 @@
 const { Router } = require('express');
 const {getVideogames} = require('../controllers/getGamesAPI&DB');
 const {getGenre} = require('../controllers/getGenresAPI&DB');
+const {getvideogames} = require('../controllers/getGamesAPI&DB');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
 
 const router = Router();
+
+router.get('/videogames', getVideogames)
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
