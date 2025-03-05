f6038570da6b3ac4af5891cee296b257cf6ee831
Erio Donalicio <erio-donalicio@hotmail.com>
2025-03-06 00:25:44 +0100

    feat: implemented genre retrieval and database synchronization

--- api/src/controllers/getGenres.js
+++ api/src/controllers/getGenres.js
@@ -5,24 +5,39 @@ const { v4: uuidv4 } = require("uuid");
 
 const url = `https://api.rawg.io/api/genres?key=${API_KEY}`;
 
+//get Genres if it doesn't exist
 const getGenres = async () => {
-  const genres = (await axios.get(url)).data.results.map((g) => ({
-    apiId: g.id,
-    id: uuidv4(),
-    name: g.name,
-    image: g.image_background,
-  }));
-  await Genres.bulkCreate(genres);
-  console.log("created genres");
+  try {
+    const { data } = await axios.get(url);
+    const genres = data.results.map((g) => ({
+      api_id: g.id,
+      id: uuidv4(),
+      name: g.name,
+      image: g.image_background,
+    }));
+
+    //avoid duplicates verifying if there are genres in DB
+    for (const genre of genres) {
+      await Genres.findOrCreate({
+        where: { api_id: genre.api_id },
+        defaults: genre,
+      });
+    }
+
+    console.log("Genres updated successfully");
+  } catch (error) {
+    console.error("Error fetching genres:", error.message);
+  }
 };
 
-const getAllGenres = (req, res) => {
-  Genres.findAll()
-    .then((response) => {
-      res.status(200).send(response);
-    })
-    .catch((err) => {
-      res.status(404).send(err);
-    });
+//function to get all genres from DB
+const getAllGenres = async (req, res) => {
+  try {
+    const genres = await Genres.findAll();
+    res.status(200).json(genres);
+  } catch (error) {
+    console.error("Error fetching genres from DB", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
-module.exports = { getGenres, getAllGenres }; 
+module.exports = { getGenres, getAllGenres };
